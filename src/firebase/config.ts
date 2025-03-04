88d34048613608b018eddefe31d8fbc7fb4410ae
Aryankaushal82 <kaushalaryan1234@gmail.com>
2025-03-04 18:51:54 +0530

    commit successfully

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCM9sE_vuF-F3T83tsr9Vr0e3O6CwS3f7s",
-  authDomain: "solar-system-5667e.firebaseapp.com",
-  projectId: "solar-system-5667e",
-  storageBucket: "solar-system-5667e.firebasestorage.app",
-  messagingSenderId: "856170873043",
-  appId: "1:856170873043:web:bb62d2a8799d88d94b5426",
-  measurementId: "G-H1MBRRG83J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
